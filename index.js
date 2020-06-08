ede78f80b9c17ec52e268a8b40b6a13d40f419a8
Nachiket More <nachiketmore99@gmail.com>
2020-06-08 17:27:42 +0530

    first deploy

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://login-pair-programming.herokuapp.com");
+    res.header("Access-Control-Allow-Origin", "https://api-pair-programming.herokuapp.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", "true")
     next();
